b6bae5835f57f9c572aca3d106efef07a3d2410f
briskled <felix.lapp@gmx.de>
2021-01-22 10:11:16 +0100

    Added first (test)-version for app and backend

--- MealSuggestionApp/App.js
+++ MealSuggestionApp/App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import * as tf from "@tensorflow/tfjs";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello people, I made an app for Meal Suggestion</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  state = {
+    isTfReady: false,
+  };
+
+  async componentDidMount() {
+    await tf.ready();
+    this.setState({ isTfReady: true });
+    console.log(this.state.isTfReady);
+  }
+
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>{this.state.isTfReady ? "Ready" : "Waiting"}</Text>
+      </View>
+    );
+  }
 }
+//https://dev.to/lankinen/setup-tensorflow-js-to-react-native-expo-project-2110
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
