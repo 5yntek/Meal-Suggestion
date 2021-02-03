1e2b884db598a578cc901d25302a3ea08c6c0854
Sebastian Heinemann <s8362356@stud.uni-frankfurt.de>
2021-02-03 12:29:47 +0100

    Flatlist in recipeScreen

--- MealSuggestionApp/src/screens/RecipeScreen.js
+++ MealSuggestionApp/src/screens/RecipeScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { View, Text , SafeAreaView, Image} from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 import Carousel from 'react-native-snap-carousel'
 
 /**
@@ -8,35 +9,63 @@ import Carousel from 'react-native-snap-carousel'
  * @param {*} props
  */
 export default function RecipeScreen(props) {
+  const renderIngredient = ({item, index}) => {
+    return(
+      <View>
+        <Text style={{width: "90%"}}>
+          {item.name + ": " + item.amount + item.unit}
+        </Text>
+      </View>
+    );
+  }
+
   const _renderItem = ({item, index}) => {
     return (
       <View style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#EDF6F9",}}>
-
+        backgroundColor: "#66CCFF",}}>
+          
         <Image 
           style={{
             alignSelf: 'center',
             height: 150,
-            width: 150,
+            width: "100%",
             borderWidth: 1,
-            borderRadius: 75
+            borderRadius: 5
           }}
           source={item.path}
-          resizeMode="stretch"
+          resizeMode="cover"   
           />
-          <Text>{item.title}</Text>
+        <Text
+          style={{
+            alignSelf: "center",
+            color: "black",
+            fontSize: 30
+          }}>
+          {item.title}
+        </Text>
+        <FlatList
+          style={{ width: "100%", margin: 10 }}
+          data={item.ingredients}
+          renderItem={renderIngredient}
+          keyExtractor={(item) => item.id}
+        /> 
       </View>
     );
   }
+
  
   const [carouselItems, setCarouselItems] = useState([
-    {title: "Gulasch", path: require('../../assets/Recipe_Props_Gulasch.jpg')},
-    {title: "Kaiserschmarrn", path: require('../../assets/Recipe_Props_Kaiserschmarrn.jpg')},
+    {title: "Gulasch mit Tütensuppe", 
+    path: require('../../assets/Recipe_Props_Gulasch.jpg'), 
+    ingredients: [{name: "Rinderfilet", unit: "g", amount: 560}, 
+                  {name: "Wasser", unit: "ml", amount: 1000}]
+    },
+    {title: "Kaiserschmarrn", path: require('../../assets/Recipe_Props_Kaiserschmarrn.jpg'), ingredients: [{name: "Eier", unit: "", amount: 3}, 
+                                                                                                            {name: "Milch", unit: "l", amount: 500}]},
     {title: "Pizza", path: require('../../assets/Recipe_Props_Pizza.jpg')},
-    {title: "Schnitzel", path: require('../../assets/Recipe_Props_Schnitzel.jpg')}
+    {title: "Schnitzel", path: require('../../assets/Recipe_Props_Schnitzel.jpg')},
+    {title: "Halber Keks", path: require('../../assets/Recipe_Props_Halber_Keks.jpeg')}
   ]);
   
   return (
@@ -50,7 +79,7 @@ export default function RecipeScreen(props) {
     >
     <Carousel
       data = {carouselItems}
-      sliderWidth={250}
+      sliderWidth={350}
       itemWidth={250}
       renderItem = {_renderItem}
       
