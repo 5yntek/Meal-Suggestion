255ace93ad5a88737ba80161b96d9300c0fcbab1
Sebastian Heinemann <s8362356@stud.uni-frankfurt.de>
2021-02-03 11:16:00 +0100

    Recipe Screen Carusel

--- MealSuggestionApp/src/screens/RecipeScreen.js
+++ MealSuggestionApp/src/screens/RecipeScreen.js
@@ -1,20 +1,54 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text , SafeAreaView, Image} from "react-native";
+import Carousel from 'react-native-snap-carousel'
 
 /**
  * Detailed description and manual for a selected recipe
  * @param {*} props
  */
-export default function SearchResultScreen(props) {
+export default function RecipeScreen(props) {
+  const _renderItem = ({item, index}) => {
+    return (
+      <View style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#EDF6F9",}}>
+
+        <Image source={item.path}/>
+          <Text>{item.title}</Text>
+      </View>
+    );
+  }
+ 
+  const [carouselItems, setCarouselItems] = useState([
+    {title: "Gulasch", path: require('../../assets/Recipe_Props_Gulasch.jpg')},
+    {title: "Kaiserschmarrn", path: require('../../assets/Recipe_Props_Kaiserschmarrn.jpg')},
+    {title: "Pizza", path: require('../../assets/Recipe_Props_Pizza.jpg')},
+    {title: "Schnitzel", path: require('../../assets/Recipe_Props_Schnitzel.jpg')}
+  ]);
+  
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#EDF6F9",
       }}
     >
-      <Text>SearchResultScreen</Text>
-    </View>
+    <Carousel
+      data = {carouselItems}
+      sliderWidth={250}
+      itemWidth={250}
+      renderItem = {_renderItem}
+      
+    />
+      
+    </SafeAreaView>
+
+
+
   );
 }
